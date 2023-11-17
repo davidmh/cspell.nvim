66e1896c6cbfe7acde9a64d2f076632cc8187d56
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2023-11-17 08:41:17 +0000

    fix: always pass title argument to vim.notify calls
    Signed-off-by: Ievgenii Shepeliuk <eshepelyuk@gmail.com>

--- lua/cspell/code_actions/make_add_to_dictionary_action.lua
+++ lua/cspell/code_actions/make_add_to_dictionary_action.lua
@@ -29,13 +29,17 @@ return function(opts)
             local dictionary_path = vim.fn.expand(opts.dictionary.path)
             local dictionary_ok, dictionary_body = pcall(vim.fn.readfile, dictionary_path)
             if not dictionary_ok then
-                vim.notify("Can't read " .. dictionary_path, vim.log.levels.ERROR)
+                vim.notify("Can't read " .. dictionary_path, vim.log.levels.ERROR, { title = "cspell.nvim" })
                 return
             end
             table.insert(dictionary_body, misspelled_word)
 
             vim.fn.writefile(dictionary_body, dictionary_path)
-            vim.notify('Added "' .. misspelled_word .. '" to ' .. opts.dictionary.path, vim.log.levels.INFO)
+            vim.notify(
+                'Added "' .. misspelled_word .. '" to ' .. opts.dictionary.path,
+                vim.log.levels.INFO,
+                { title = "cspell.nvim" }
+            )
 
             -- replace word in buffer to trigger cspell to update diagnostics
             h.set_word(opts.diagnostic, opts.word)
