c423159fd98d215782fa72fd7e99821cca4142f4
Nate Jensvold <jensvoldnate@gmail.com>
2024-06-02 22:35:33 -0700

    feat: add support for using multiple cspell.json as config sources

--- lua/cspell/code_actions/make_add_to_json.lua
+++ lua/cspell/code_actions/make_add_to_json.lua
@@ -4,6 +4,7 @@ local h = require("cspell.helpers")
 ---@class AddToJSONAction
 ---@field diagnostic Diagnostic
 ---@field word string
+---@field cspell_config_path string
 ---@field params GeneratorParams
 
 ---@param opts AddToJSONAction
@@ -17,22 +18,28 @@ return function(opts)
     local misspelled_word = opts.diagnostic.user_data.misspelled
 
     return {
-        title = 'Add "' .. misspelled_word .. '" to cspell json file',
+        title = h.format(
+            'Add "${word}" to "${config_path}"',
+            { word = misspelled_word, config_path = h.shorten_path(opts.cspell_config_path) }
+        ),
+
         action = function()
+            local cspell_config_path = opts.cspell_config_path
             -- get a fresh config when the action is performed, which can be much later than when the action was generated
-            local cspell = h.async_get_config_info(opts.params)
-            local config_path = h.get_config_path(opts.params)
-            if not cspell and config_path and Path:new(config_path):exists() then
+            local cspell = h.async_get_config_info(opts.params, cspell_config_path)
+            local path_exists = Path:new(cspell_config_path):exists()
+            if not cspell and path_exists then
                 h.cache_word_for_json(misspelled_word)
                 return
             end
-            cspell = cspell or h.create_cspell_json(opts.params)
+
+            cspell = cspell or h.create_default_cspell_json(opts.params, cspell_config_path)
 
             if not cspell.config.words then
                 cspell.config.words = {}
             end
 
-            h.add_words_to_json(opts.params, { misspelled_word })
+            h.add_words_to_json(opts.params, { misspelled_word }, cspell_config_path)
 
             -- replace word in buffer to trigger cspell to update diagnostics
             h.set_word(opts.diagnostic, opts.word)
